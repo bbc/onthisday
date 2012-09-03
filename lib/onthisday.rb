b67373a812d43f84ed2808de8ab7d16d4300b970
Chris Lowis <chris.lowis@gmail.com>
2012-09-03 13:50:57 +0100

    initial commit

---
+++ lib/onthisday.rb
@@ -0,0 +1,79 @@
+# encoding: utf-8
+
+require 'rest_client'
+require 'nokogiri'
+
+module OnThisDay
+  class Item
+    def initialize(element)
+      @element = element
+      @year = nil
+      remove_noprint_elements!
+      set_and_remove_year!
+    end
+
+    # Remove any child nodes with class "nopront". This removes the
+    # boilerplate Wikinews, Obituries etc.
+    def remove_noprint_elements!
+      @element.xpath('//*[starts-with(@class,"noprint")]').each do |node|
+        node.children.remove
+      end
+    end
+
+    def year
+      @year.to_i
+    end
+
+    def set_and_remove_year!
+      @element.xpath('./a').each do |node|
+        title = node['title']
+
+        # if the title of the link looks like a year, e.g. "1879", set
+        # the year of this item and remove the node
+        if title.match /\A\d{4,4}\z/
+          @year = title
+          node.remove
+        end
+      end
+    end
+
+    def text
+      @element.inner_text.gsub(' – ','')
+    end
+
+    def html
+      @element.inner_html.gsub(' – ','')
+    end
+
+    # Rescursively search for all a elements in this element and
+    # return their value (removing /wiki/)
+    def topics
+      @element.xpath('.//a').map do |a|
+        a.attr('href').gsub('/wiki/','')
+      end
+    end
+  end
+
+  class Parser
+    def initialize
+    end
+
+    def items
+      elements = doc.xpath("//div[@id='mp-otd']/ul/li")
+      elements.map {|e| Item.new(e)}
+    end
+
+    def doc
+      Nokogiri::HTML(content)
+    end
+
+    def wikipedia_url
+      "http://en.wikipedia.org/wiki/Main_Page"
+    end
+
+    def content
+      RestClient.proxy = ENV['http_proxy']
+      @content ||= RestClient.get wikipedia_url
+    end
+  end
+end
---
+++ test/onthisday_test.rb
@@ -0,0 +1,43 @@
+require_relative '../lib/onthisday'
+require 'minitest/autorun'
+require 'webmock/minitest'
+
+require 'webmock'
+
+class TestOnThisDay < MiniTest::Unit::TestCase
+  def fixture_file
+    fn = File.expand_path(File.join(File.dirname(__FILE__), 'fixtures', 'main_page_20120620.html'))
+    File.open(fn, "rb").read
+  end
+
+  def setup
+    @on_this_day = OnThisDay::Parser.new
+    stub_request(:get, "en.wikipedia.org/wiki/Main_Page").
+      to_return({:body => fixture_file})
+
+  end
+
+  def test_returns_correct_number_of_items
+    assert_equal 5, @on_this_day.items.size
+  end
+
+  def test_returns_text_of_news_headline
+    assert_equal "French Revolution: Meeting on a tennis court near the Palace of Versailles, members of France's Third Estate took the Tennis Court Oath, pledging not to separate until a new constitution was established.", @on_this_day.items[0].text
+  end
+
+  def test_returns_html_of_news_headline
+    assert_equal "<a href=\"/wiki/French_Revolution\" title=\"French Revolution\">French Revolution</a>: Meeting on a tennis court near the <a href=\"/wiki/Palace_of_Versailles\" title=\"Palace of Versailles\">Palace of Versailles</a>, members of France's <a href=\"/wiki/Estates_of_the_realm\" title=\"Estates of the realm\">Third Estate</a> took the <b><a href=\"/wiki/Tennis_Court_Oath\" title=\"Tennis Court Oath\">Tennis Court Oath</a></b>, pledging not to separate until a new <a href=\"/wiki/Constitution\" title=\"Constitution\">constitution</a> was established.", @on_this_day.items[0].html
+  end
+
+  def test_returns_year_of_item
+    assert_equal 1789, @on_this_day.items[0].year
+  end
+
+  def test_returns_topics_contained_in_news_headline
+    item = @on_this_day.items[0]
+
+    ['French_Revolution', 'Palace_of_Versailles', 'Estates_of_the_realm', 'Tennis_Court_Oath', 'Constitution'].each do |topic|
+      assert item.topics.include? topic
+    end
+  end
+end
